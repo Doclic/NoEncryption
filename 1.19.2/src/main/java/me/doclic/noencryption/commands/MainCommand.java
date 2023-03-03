cdc7e269159947fa1b0ace0ceea0c5c4e4afeee2
Vincent <dev.v1nc3ntwastaken@gmail.com>
2023-03-03 11:19:51 -0600

    Added a command to manually check for new updates, and added a forgotten class in 1.19.3

--- 1.19.2/src/main/java/me/doclic/noencryption/commands/MainCommand.java
+++ 1.19.2/src/main/java/me/doclic/noencryption/commands/MainCommand.java
@@ -2,11 +2,13 @@ package me.doclic.noencryption.commands;
 
 import me.doclic.noencryption.Chat;
 import me.doclic.noencryption.config.ConfigurationHandler;
+import me.doclic.noencryption.utils.updates.UpdateChecker;
 import org.bukkit.ChatColor;
 import org.bukkit.command.*;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -20,8 +22,10 @@ public class MainCommand implements CommandExecutor, TabCompleter {
             // Send help options depending on if they are console or not
             if (sender instanceof ConsoleCommandSender) {
                 Chat.sendChat(sender, "&7 suppressnotices &8-&7 Suppress startup config notices");
+                Chat.sendChat(sender, "&7 checkforupdates &8-&7 Queries the GitHub for an updated version of NoEncryption");
             } else {
-
+                if (sender.hasPermission("noencryption.command.suppressnotices"))
+                    Chat.sendChat(sender, "&7 checkforupdates &8-&7 Queries the GitHub for an updated version of NoEncryption");
             }
 
             return true;
@@ -46,6 +50,31 @@ public class MainCommand implements CommandExecutor, TabCompleter {
                         Chat.sendChat(sender, "&c[NoEncryption] There are no active config messages to suppress");
 
                     break;
+                case "checkforupdates":
+                    if (!sender.hasPermission("noencryption.command.suppressnotices")) {
+                        Chat.sendChat(sender, "&c[NoEncryption] You do not have permission to run this command!");
+                        return true;
+                    }
+
+                    Chat.sendChat(sender, "&c[NoEncryption] &7Checking for updates...");
+
+                    UpdateChecker.check(
+                            () -> {
+                                Chat.sendChat(sender, "&c[NoEncryption] &7There is a new update available. You can download it here:");
+                                Chat.sendChat(sender, UpdateChecker.updateUrl.toString());
+                            },
+                            () -> {
+                                Chat.sendChat(sender, "&c[NoEncryption] &7You are on the latest stable NoEncryption build");
+                            },
+                            () -> {
+                                Chat.sendChat(sender, "&c[NoEncryption] There was an error checking for a new update.");
+                                Chat.sendChat(sender, "If you are a server administrator, check your server's internet connection,");
+                                Chat.sendChat(sender, "Or check the GitHub Status Page for API Requests issues: ");
+                                Chat.sendChat(sender, "https://www.githubstatus.com/");
+                            }
+                    );
+
+                    break;
                 default:
                     Chat.sendChat(sender, "&c[NoEncryption] Invalid argument \"" + ChatColor.stripColor(args[0]) + "\"!");
             }
@@ -66,11 +95,17 @@ public class MainCommand implements CommandExecutor, TabCompleter {
             } else if (args.length == 1) {
                 if (sender instanceof ConsoleCommandSender)
                     return TabUtils.match(args[0], Arrays.asList(
-                        "suppressnotices"
+                            "suppressnotices",
+                            "checkforupdates"
                     ));
-                else
+                else {
+                    List<String> returnStrings = new ArrayList<>();
                     // Can swap for above for new commands that players can run
-                    return Collections.emptyList();
+                    if (sender.hasPermission("noencryption.command.suppressnotices"))
+                        returnStrings.add("checkforupdates");
+
+                    return TabUtils.match(args[0], returnStrings);
+                }
             } else {
                 return Collections.emptyList();
             }
